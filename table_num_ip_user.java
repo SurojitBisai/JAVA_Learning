8ab27775277a75cdf53022f4a3c0094d04e33fd1
surojit Bisai <surojitbisai8328@gmail.com>
2025-09-09 22:07:57 +0530

    4 * 5 print pattern

--- table_num_ip_user.java
+++ table_num_ip_user.java
@@ -1,18 +1,13 @@
 
-// import  java.util.*;
-// public class table_num_ip_user {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-
-//         for(int i = 1; i < 11; i++){
-//             System.out.println(i*n);
-//         }
-//     }
-// }
-public class Geeks {
-    static int $count = 5;
+import  java.util.*;
+public class table_num_ip_user {
     public static void main(String[] args) {
-        System.out.println($count);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i < 11; i++){
+            System.out.println(i*n);
+        }
     }
 }
+

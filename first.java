f597b96483ea279b175e9d4ff6f72924289200a7
Surojit Bisai <surojitbisai8328@gmail.com>
2025-11-12 19:15:03 +0530

    hello

---
+++ first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String[] args) {
+System.out.print("hi\n");
+System.out.print("Hello World with jaca\n");
+
+System.out.print("Hello World with jaca");
+}
+}

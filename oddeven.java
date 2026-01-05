e2ec664241c8cc1cffb94b258d0b35c89f5cae2a
Surojit Bisai <surojitbisai8328@gmail.com>
2026-01-05 21:12:07 +0530

    oddeven

--- oddeven.java
+++ oddeven.java
@@ -5,10 +5,10 @@ public class oddeven {
     
     public static void main(String[] args) {
         
-        Scanner sc= new Scanner(System.in);
+        Scanner SC= new Scanner(System.in);
         int x;
         System.out.println("Enter the number:-");
-        x = sc.nextInt();
+        x = SC.nextInt();
 
         if(x % 2 == 0) {
             System.out.println( "Even");
